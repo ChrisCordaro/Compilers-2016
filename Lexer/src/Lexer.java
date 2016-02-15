b8c6c4a0f4ef5ed5f6ce5d8385da865b781e4d6b
ChrisCordaro <christopher.cordaro2@marist.edu>
2016-02-14 21:46:29 -0500

    Tokens Complete
    Got the tokens to add to the token array along with their value and
    type. Accomplished this by using a counter which will run through the
    regex, and count the number in the regex list where the match is found,
    and then pass that through to the new token. The when a token is created
    the createToken method is ran to see where the match took place, and
    then assigns the token type accordingly. However, I still need to figure
    out the line numbers.

--- Lexer/src/Lexer.java
+++ Lexer/src/Lexer.java
@@ -1,22 +1,48 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
+	//Keywords
 	private static String intPattern = "(int)";
+	private static String stringPattern = "(string)";
+	private static String booleanPattern = "(boolean)";
+	private static String ifPattern = "(if)";
+	private static String whilePattern = "(while)";
+	private static String printPattern = "(print)";
+	
+
+	
+	
+	
+	private static String assign = "(=)";
+	private static String digit = "(\\d)";
 	private static String alpha = "([a-z])";
 	private static String space = "(\\s)";
+	private static String leftParen = "(()";
+	private static String rightParen = "())";
+	private static String leftBrace = "({)";
+	private static String rightBrace = "(})";
+	private static String quote = "(\")";
 	
-	private static String test = "(int)|([a-z])";
+	private static String equality = "(==)";
+	private static String notEqual = "(!=)";
+	private static String intOp = "(+)";
 	
-	private static ArrayList<Character> intArray = new ArrayList<Character>();
+	//private static String test = "(int)|([a-z])";
+	
+	private static ArrayList<Character> charArray = new ArrayList<Character>();
 	private static ArrayList<Token> tokenArray = new ArrayList<Token>();
-	private static ArrayList<String> testArray = new ArrayList<String>();
+	private static ArrayList<String> validArray = new ArrayList<String>();
 	
 	public static void main(String[] args) throws IOException {
 		FileInputStream fileInput = new FileInputStream("C:/Users/Chris/Desktop/test.txt");
+		
+		
 
 		ArrayList<Character> charArray = new ArrayList<Character>();
 
@@ -29,20 +55,26 @@ public class Lexer {
 
 		}
 		//HOW TO LOOP THIS
+		
 		testForSpace(charArray);
-		System.out.println(analyzeList(intArray));
-		testString(analyzeList(intArray));
+		System.out.println(analyzeList(charArray));
+		testString(analyzeList(charArray));
 		//makeTokens(testArray);
-		makeTokens(testArray);
+		//makeTokens(validArray);
+		//setTokenPos(validArray);
+		//System.out.println(tokenArray);
+		System.out.println("Here are the valid characters that will be analyzed " + validArray);
 		for(int i = 0; i < tokenArray.size(); i ++){
+		
+			//System.out.println("Token " + i + " : " + tokenArray.get(i).getValue());
 			System.out.println("Token " + i + " : " + tokenArray.get(i).getType());
 		}
 	
+		//System.out.println(tokenArray.get(2).getType());
 		
 		fileInput.close();
 		
 		
-
 	}
 
 	// Iterates through arraylist creating a new array list of characters
@@ -50,18 +82,30 @@ public class Lexer {
 	public static void testForSpace(ArrayList<Character> x) {
 		// ArrayList<Character> intArray = new ArrayList<Character>();
 		// Pattern p = Pattern.compile(space);
+		int counter = 1;
 		for (int i = 0; i < x.size(); i++) {
+			
+			
 
 			if (x.get(i) != ' ') {
 
-				intArray.add(x.get(i));
+				charArray.add(x.get(i));
 
+			}
+			
+			if(x.get(i) == '\n'){
+				System.out.println("ON LINE " + counter);
+				
+				counter++;
+				//intArray.add(x.get(i));
+				
 			}
 
 			if (x.get(i) == ' ') {
-				System.out.println(intArray);
-
-				break;
+				System.out.println("Valid characters");
+				System.out.println(charArray);
+				
+				
 			}
 		}
 	}
@@ -80,49 +124,80 @@ public class Lexer {
 	// Remember longest match
 	public static ArrayList<String> testString(String s) {
 		
-		Pattern r = Pattern.compile(intPattern);
-		Matcher m = r.matcher(s);
+		/*private static String intPattern = "(int)";
+		private static String stringPattern = "(string)";
+		private static String booleanPattern = "(boolean)";
+		private static String ifPattern = "(if)";
+		private static String whilePattern = "(while)";
+		private static String printPattern = "(print)";
 		
-		Pattern r1 = Pattern.compile(alpha);
-		Matcher m1 = r1.matcher(s);
 		
-		Pattern r2 = Pattern.compile(test);
-		Matcher m2 = r2.matcher(s);
 		
+		private static String assign = "(=)";
+		private static String digit = "(\\d)";
+		private static String alpha = "([a-z])";
+		private static String space = "(\\s)";
+		private static String leftParen = "(()";
+		private static String rightParen = "())";
+		private static String leftBrace = "({)";
+		private static String rightBrace = "(})";
+		private static String quote = "(\")";
+		
+		private static String equality = "(==)";
+		private static String notEqual = "(!=)";
+		private static String intOp = "(+)";*/
 		
 	
-		if(m.find()){
-			System.out.println(m);
-			System.out.println("I FOUND AN INT");
-			//testArray.add(m.group());
-			System.out.println(testArray);
 		
-		}
-		if(m1.find()){
-			System.out.println(m1);
-			System.out.println("I FOUND AN ALPHA");
-			//testArray.add(m1.group());
-			System.out.println(testArray);
-		}
+		Pattern r2 = Pattern.compile(printPattern + "|" + whilePattern + "|" + intPattern + "|" + ifPattern + "|" + stringPattern + "|" + booleanPattern + "|" + equality +  
+				 "|" + notEqual  + "|"  + alpha);
 		
-		//in the example of xint this will find x and stop looking at the string
-		//Figure out a way to get this to loop and not look at the thing already found
-		/*if(m2.find()){
-			System.out.println("MATCH INT/CHAR FOUND");
-			
-			testArray.add(m2.group());
-			System.out.println(testArray);
-		}*/
+		
+		Matcher m2 = r2.matcher(s);
+		
+		Token token;
+		int counter = 0;
+		boolean found = false;
+		
 		while(m2.find()){
-			testArray.add(m2.group());
-			System.out.println(testArray);
+			while(counter < 11 && !found){
+				if(m2.group(counter) != null){
+					validArray.add(m2.group());
+					System.out.println("valid array");
+					System.out.println(validArray);
+					tokenArray.add(token = new Token(counter, m2.group(counter), 1));
+					found = true;
+				}
+				counter++;
+				
+			}
+			counter = 1;
+			found = false;
 		}
-		return testArray;
-	}
+		//return tokenArray;
+		return validArray;
+	};
+
+		
+		
+	//Need to figure out how to add line number. 
+		/*while(m2.find()){
+			Token token = new Token();
+			//token.setType("keyword");
+			token.setType("chicken");
+			tokenArray.add(token);
+			validArray.add(m2.group());
+			System.out.println(validArray);
+		}
+		return validArray;
+	} //This chunk of code currently works but not perfect
+		
 	
 	
 	//take in the testArray and tokenize it
-	public static ArrayList<Token> makeTokens(ArrayList<String> a){
+	//Might have to combine this method with testString so when m.find() add the token immediatley 
+	
+	/*public static ArrayList<Token> makeTokens(ArrayList<String> a){
 		for(int i = 0; i < a.size(); i++){
 			Token token = new Token();
 			token.setPosition(i);
@@ -130,6 +205,16 @@ public class Lexer {
 			tokenArray.add(token);
 		}
 		return tokenArray;
+	}*/
+	
+	//Idea of this is to read in the token list and analyze what kind of token it is
+	public static void analyzeTokenList(ArrayList<Token> t){
+		for(int i = 0; i < t.size(); i++){
+			if(t.get(i)){
+				
+			}
+		}
 	}
+	
 
 }
--- Lexer/src/Token.java
+++ Lexer/src/Token.java
@@ -2,12 +2,58 @@
 public class Token {
 	private String type;
 	private int position;
+	private String value;
 	
 	public Token(){
 		type = null;
 		position = 0;
 	}
+	//group is the group that the matcher has successfully matched on defined int
+	public Token(int group, String tokenVal, int lineNum){
+		value = tokenVal;
+		position = lineNum;
+		createToken(group);
+	}
 	
+	public void createToken(int group){
+		switch(group){
+		case 1:
+			type = "printWord";
+			break;
+		case 2:
+			type = "whileWord";
+			break;
+		case 3:
+			type = "intWord";
+			break;
+		case 4:
+			type = "ifWord";
+			break;
+		case 5:
+			type = "stringWord";
+			break;
+		case 6:
+			type = "booleanWord";
+			break;
+		case 7:
+			type = "equality";
+			break;
+		case 8:
+			type = "nonEquality";
+			break;
+		case 9:
+			type = "alpha";
+			break;
+		}
+	}
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 	public Token(String s, int x){
 		type = s;
 		position = x;
